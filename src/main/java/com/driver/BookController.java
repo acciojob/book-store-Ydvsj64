a3c8f5bf403ff1950003c6e0b23993594692c864
Ydvsj64 <sarvjeetyadavcse2019@gmail.com>
2022-12-03 00:12:05 +0530

    book store

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,21 +3,57 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
 public class BookController {
+//   static int id=0;
+
+
     @Autowired
     BookService bookService;
 
     // One example controller, make the rest by yourself
     @PostMapping("/create-book")
     public ResponseEntity createBook(@RequestBody Book book){
+//        id++;
+//        book.setId(id);
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get-book-by-id/{id}")
+    public ResponseEntity<Book> findBookById(@PathVariable("id") String id){
+        return new ResponseEntity<>(bookService.findBookById(id),HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/get-all-books")
+    public ResponseEntity<List<Book>> findAllBooks(){
+        return new ResponseEntity<>(bookService.findAllBooks(),HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/get-books-by-author")
+    public ResponseEntity<List<Book>> findBooksByAuthor(@RequestParam String author){
+        return new ResponseEntity<>(bookService.findBooksByAuthor(author),HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/get-books-by-genre")
+    public ResponseEntity<List<Book>> findBooksByGenre(@RequestParam String genre){
+        return new ResponseEntity<>(bookService.findBooksByGenre(genre),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/delete-book-by-id/{id}")
+    public ResponseEntity deleteBookById(@PathVariable("id") String id){
+        bookService.deleteBookById(id);
+        return new ResponseEntity<>(HttpStatus.FOUND);
+    }
+
+    @DeleteMapping("/delete-all-books")
+    public ResponseEntity deleteAllBooks(){
+        bookService.deleteAllBooks();
+        return new ResponseEntity<>(HttpStatus.FOUND);
     }
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -6,36 +6,54 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BookRepository {
 
+    List<Book> bookList;
+    static int id=0;
     public BookRepository(){
-        
+        bookList=new ArrayList<>();
     }
 
     public Book save(Book book){
-        return null;
+        id++;
+        book.setId(id);
+        bookList.add(book);
+        return book;
     }
 
     public Book findBookById(int id){
-
+        for(Book b:bookList){
+            if(b.getId()==id)return b;
+        }
         return null;
+//        return bookList.get(id-1);
     }
 
     public List<Book> findAll(){
-        return null;
+        return bookList;
     }
 
     public void deleteBookById(int id){
-        return;
+        for(int i=0;i<bookList.size();i++){
+            if(bookList.get(i).getId()==id)bookList.remove(i);
+        }
     }
 
     public void deleteAll(){
-        return;
+        bookList.clear();
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book> authorBookList=new ArrayList<>();
+        for(Book b:bookList){
+            if(b.getAuthor().equals(author)) authorBookList.add(b);
+        }
+        return authorBookList;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+        List<Book> genreBookList=new ArrayList<>();
+        for(Book b:bookList){
+            if(b.getGenre().equals(genre)) genreBookList.add(b);
+        }
+        return genreBookList;
     }
 }
